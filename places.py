6bc71c7e36124c5d0a46c40c8c4db7f0574a7707
sakurum <sakuranomiyu@gmail.com>
2020-09-29 11:51:36 +0900

    場所から取得する

---
+++ places.py
@@ -0,0 +1,44 @@
+search_places = [
+    {
+        "name": "fukuoka",
+        "query": "",
+        "latitude": 33.60639,
+        "longitude": 130.41806,
+        "range": "100km"
+    },
+    {
+        "name": "osaka",
+        "query": "",
+        "latitude": 33.60639,
+        "longitude": 130.41806,
+        "range": "100km"
+    },
+    {
+        "name": "tokyo",
+        "query": "",
+        "latitude": 33.60639,
+        "longitude": 130.41806,
+        "range": "100km"
+    },
+    {
+        "name": "sendai",
+        "query": "",
+        "latitude": 33.60639,
+        "longitude": 130.41806,
+        "range": "100km"
+    },
+    {
+        "name": "sapporo",
+        "query": "",
+        "latitude": 33.60639,
+        "longitude": 130.41806,
+        "range": "100km"
+    },
+    {
+        "name": "japan",
+        "query": "頭痛",
+        "latitude": 35,
+        "longitude": 135,
+        "range": "1500km"
+    }
+]
