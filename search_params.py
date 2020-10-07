cf9e0cc1e48a25b1c2a1cc4f9543a7c380be2f01
sakurum <sakuranomiyu@gmail.com>
2020-10-07 14:43:01 +0900

    空白を除去

--- search_params.py
+++ search_params.py
@@ -3,7 +3,7 @@ search_params = [
         "collection_name": "okinawa_r10km",
         "params": {
             "q": "",
-            "geocode": "26.2125, 127.68111, 10km",
+            "geocode": "26.2125,127.68111,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -15,7 +15,7 @@ search_params = [
         "collection_name": "fukuoka_r10km",
         "params": {
             "q": "",
-            "geocode": "33.60639, 130.41806, 10km",
+            "geocode": "33.60639,130.41806,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -27,7 +27,7 @@ search_params = [
         "collection_name": "osaka_r10km",
         "params": {
             "q": "",
-            "geocode": "34.68639, 135.52, 10km",
+            "geocode": "34.68639,135.52,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -39,7 +39,7 @@ search_params = [
         "collection_name": "tokyo_r5km",
         "params": {
             "q": "",
-            "geocode": "35.68944, 139.69167, 10km",
+            "geocode": "35.68944,139.69167,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -51,7 +51,7 @@ search_params = [
         "collection_name": "sendai_r10km",
         "params": {
             "q": "",
-            "geocode": "38.26889, 140.87194, 10km",
+            "geocode": "38.26889,140.87194,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -63,7 +63,7 @@ search_params = [
         "collection_name": "aomori_r10km",
         "params": {
             "q": "",
-            "geocode": "40.82444, 140.74, 10km",
+            "geocode": "40.82444,140.74,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
@@ -75,60 +75,12 @@ search_params = [
         "collection_name": "sapporo_r10km",
         "params": {
             "q": "",
-            "geocode": "43.06417, 141.34694, 10km",
+            "geocode": "43.06417,141.34694,10km",
             "count": 100,
             "result_type": "recent",
             "exclude": "retweets",
             "lang": "ja",
             "locale": "ja"
         }
-    },
-
-
-]
-
-search_places = [
-
-    {
-        "name": "hiroshima",
-        "query": "",
-        "latitude": 34.39639,
-        "longitude": 132.45944,
-        "range": "10km"
-    },
-    {
-        "name": "osaka",
-        "query": "",
-        "latitude": 34.68639,
-        "longitude": 135.52,
-        "range": "10km"
-    },
-    {
-        "name": "tokyo",
-        "query": "",
-        "latitude": 35.68944,
-        "longitude": 139.69167,
-        "range": "10km"
-    },
-    {
-        "name": "sendai",
-        "query": "",
-        "latitude": 38.26889,
-        "longitude": 140.87194,
-        "range": "10km"
-    },
-    {
-        "name": "aomori",
-        "query": "",
-        "latitude": 40.82444,
-        "longitude": 140.74,
-        "range": "10km"
-    },
-    {
-        "name": "sapporo",
-        "query": "",
-        "latitude": 43.06417,
-        "longitude": 141.34694,
-        "range": "10km"
     }
 ]
